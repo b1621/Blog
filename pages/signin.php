69220abb05e126e289375487e5ef78a179f836e1
Abukiii <abubekerabdu85@gmail.com>
2022-11-15 22:36:37 +0300

    Set up Signin process for the user

--- pages/signin.php
+++ pages/signin.php
@@ -10,27 +10,29 @@
 </head>
 <body>
 
-<form  action="signup_process.php" method="POST">
+<form  action="../process/signin_process.php" method="POST">
 <div class="container">
   <div class="row justify-content-center">
   <div class="col-md-5">
    <div class="card">
      <h2 class="card-title text-center">Login </h2>
+     
+
       <div class="card-body py-md-4">
        <form _lpchecked="1">
           
         <div class="form-group">
-             <input type="email" class="form-control" id="email" placeholder="Email">
+             <input type="email" class="form-control" name ="email" id="email" placeholder="Email">
                             </div>
                             
                           
    <div class="form-group">
-     <input type="password" class="form-control" id="password" placeholder="Password">
+     <input type="password" class="form-control" name = "password" id="password" placeholder="Password">
    </div>
    
    <div class="d-flex flex-row align-items-center justify-content-between">
       <a href="signup.php">Create Account</a>
-        <button  class="btn btn-primary" value = "SignUp" name="SignUp">Login</button>
+        <button  class="btn btn-primary" value = "Login" name="Login">Login</button>
           </div>
        </form>
      </div>
