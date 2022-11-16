74c630e812f1e381e3be0fffd69bdeaa9d2fa81f
B <lijetheal12@gmail.com>
2022-11-16 09:16:30 +0300

    update user_home.php

--- pages/user_home.php
+++ pages/user_home.php
@@ -4,11 +4,11 @@
 
 
 // if(isset($_SESSION['ID'])){
-    // echo 'welcome' . $_SESSION['ID'].'<br/>';
-    // echo ' <a href = "../process/logout.php?logout">Logout</a>';
+// echo 'welcome' . $_SESSION['ID'].'<br/>';
+// echo ' <a href = "../process/logout.php?logout">Logout</a>';
 // }
 // else{
-    // header("location:signin.php");
+// header("location:signin.php");
 // }
 ?>
 <?php
@@ -21,6 +21,7 @@ $result = $statement->fetchall(PDO::FETCH_ASSOC);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,19 +29,23 @@ $result = $statement->fetchall(PDO::FETCH_ASSOC);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
-<div class="container border pt-4" style="">
 
-<div class="card mb-3" style="width :60%; margin:0 auto;">
-    <div class="card-body">
-        <h5 class="card-title">Title</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Author</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
+<body>
+    <div class="container border pt-4" style="">
+
+        <?php foreach ($result as $i => $blog) : ?>
+            <div class="card mb-3" style="width :60%; margin:0 auto;">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $blog['Title'];  ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $blog['Author'];  ?></h6>
+                    <p class="card-text"><?php echo $blog['Article'];  ?></p>
+                    <p class="card-text"><small class="text-muted"><?php echo $blog['Date'];  ?></small></p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        <?php endforeach ?>
     </div>
-</div>
-</div>
 </body>
+
 </html>
