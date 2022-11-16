52d9ee154fa202e30c39a0c27b2c97850c97e853
B <lijetheal12@gmail.com>
2022-11-16 14:17:09 +0300

    update signin and user_home

--- pages/user_home.php
+++ pages/user_home.php
@@ -3,14 +3,13 @@
 session_start();
 
 
-// if(isset($_SESSION['ID'])){
-// echo 'welcome' . $_SESSION['ID'].'<br/>';
-// echo ' <a href = "../process/logout.php?logout">Logout</a>';
-// }
-// else{
-// header("location:signin.php");
-// }
-// 
+if (isset($_SESSION['ID'])) {
+    echo 'welcome' . $_SESSION['ID'] . '<br/>';
+    echo ' <a href = "../process/logout.php?logout">Logout</a>';
+} else {
+    header("location:signin.php");
+}
+
 ?>
 <?php
 include_once "../process/db_connection.php";
