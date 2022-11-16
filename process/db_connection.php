0155903a4c468089c19096c2c403b46312a10892
Abukiii <abubekerabdu85@gmail.com>
2022-11-16 14:08:29 +0300

    Change Empty Field Error FOr Signin

--- process/db_connection.php
+++ process/db_connection.php
@@ -5,6 +5,10 @@ $password = '';
 $host = "localhost:3306"; 
 $dbname = 'crud'; 
 
-//$pdo = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
+$pdo = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $con = mysqli_connect($host, $username, $password, $dbname);
-//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if(!$con){
+    echo 'please check your database connection';
+}
+echo "Connected";
