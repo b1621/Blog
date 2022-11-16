e4fc9c593de0c3051ab753b8cfa326d872509f34
B <lijetheal12@gmail.com>
2022-11-16 07:53:19 +0300

    for the time

--- pages/create_blog.php
+++ pages/create_blog.php
@@ -1,3 +1,37 @@
+<?php
+
+// include_once './db_connection.php';
+include_once '../process/db_connection.php';
+//  change the error type into object and assign every error to its key which is the name
+$error = [];
+$title = $_POST['title'];
+$author = $_POST['author'];
+$date = date('y-m-d');
+$article = nl2br($_POST['article']);
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(!$_POST['title']){
+        $error['title'] = 'title is required';
+    }
+    if(!$_POST['author']){
+        $error['author'] = 'author is required';
+    }
+    if(!$_POST['article']){
+        $error['article'] = 'article is required';
+    }
+    if(empty($error)){
+        $statment = $pdo->prepare('INSERT INTO blog (Title, Author, Article, Date) VALUES(:title, :author, :article, :date)');
+        $statment->bindValue(':title',$title);
+        $statment->bindValue(':article',$article);
+        $statment->bindValue(':author',$author);
+        $statment->bindValue(':date',$date);
+        $statment->execute();
+        header('Location: ../pages/blogs.php');
+        exit();
+    }
+}
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -15,12 +49,24 @@
   <div class="container ">
         <div class="border p-5" style="width: 60%; margin:4% auto;">
         <div>
-            <a href="" style="float:right;" type="button" class="btn-close" aria-label="Close"></a>
+            <a href="../pages/blogs.php" style="float:right;" type="button" class="btn-close" aria-label="Close"></a>
         </div>
         <div class="mb-4">
             <h3>Create a Blog</h3>
         </div>
-             <form action="../process/create_blog.inc.php" method="post">
+            <?php
+            echo sizeof($error);
+            print_r($error);
+            if(!empty($error)): 
+            ?>
+                    <div class="error">
+                         <?php // foreach error in ?>
+                        <?php // endforeach;?>
+                        there is error 
+                    </div>
+            <?php  endif; ?>
+             <!-- <form action="../process/create_blog.inc.php" method="post"> -->
+                <form action="" method="post">
   <!-- Title input -->
   <div class="form-outline mb-4">
     <input type="text" id="form4Example1" name="title" class="form-control" placeholder="Title" />
@@ -39,7 +85,7 @@
   </div>
 
   <!-- Submit button -->
-  <div class="" style="">
+  <div class="">
     <button type="submit" class="btn btn-primary btn-block  mb-4" style="width: 70%;margin:0 15%;">Publish</button>
   </div>
 </form>
