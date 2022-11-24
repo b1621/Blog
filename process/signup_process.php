f30067a09843467901b16fd7cd83ac76a08d6eb6
B <lijetheal12@gmail.com>
2022-11-24 10:18:40 +0300

    adding admin start page

--- process/signup_process.php
+++ process/signup_process.php
@@ -7,40 +7,32 @@ $error = [];
 $name = $_POST['name'];
 $email = $_POST['email'];
 $date = date('y-m-d');
+$con_pass = $_POST['confirm-password'];
 $password = $_POST['password'];
+
 $encPass = md5($password);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (!$_POST['name']) {
-        $error[] = 'Name is required';
-    }
-    if (!$_POST['password']) {
-        $error[] = 'Email is required';
-    }
-    if (!$_POST['email']) {
-        $error[] = 'Email is required';
-    }
-    if (!$_POST['password']) {
-        $error[] = 'confirm-password is required';
-    }
-    if (empty($error)) {
 
-        $query = "SELECT * FROM user_security WHERE Email = '" . $_POST['email'] . "' ";
-        $result = mysqli_query($con, $query);
 
-        if ($row = mysqli_fetch_assoc($result)) {
-            $error[] = 'Already have an account';
-            header('Location: ../pages/signin.php?user= Already have an account');
-        } else {
-            $statment = $pdo->prepare('INSERT INTO user_security (User_name, Email, Password, Date) VALUES(:User_name, :Email, :Password, :date)');
-            $statment->bindValue(':User_name', $name);
-            $statment->bindValue(':Email', $email);
-            $statment->bindValue(':Password', $encPass);
-            $statment->bindValue(':date', $date);
-            $statment->execute();
+    $query = "SELECT * FROM user_security WHERE Email = '" . $_POST['email'] . "' ";
+    $result = mysqli_query($con, $query);
 
-            header('Location: ../pages/signin.php?success= Account Created Successfully');
-            exit();
-        }
+    if ($row = mysqli_fetch_assoc($result)) {
+        $error[] = 'Already have an account';
+        header('Location: ../pages/signin.php?error= Already have an account');
+    } elseif ($password != $con_pass) {
+        header('Location: ../pages/signin.php?error= password doesnot match');
+        // echo 'password not equal';
+    } else {
+        $statment = $pdo->prepare('INSERT INTO user_security (User_name, Email, Password, Date) VALUES(:User_name, :Email, :Password, :date)');
+        $statment->bindValue(':User_name', $name);
+        $statment->bindValue(':Email', $email);
+        $statment->bindValue(':Password', $encPass);
+        $statment->bindValue(':date', $date);
+        $statment->execute();
+
+        header('Location: ../pages/signin.php?success= Account Created Successfully');
+        exit();
     }
 }
