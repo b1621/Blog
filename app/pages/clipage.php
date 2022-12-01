8fbbe5fe1b175476e19f1e6f265bc43caec0206f
Alazar dinberu <alazardinberu64@gmail.com>
2022-12-01 11:55:47 +0300

    modified clipage

--- app/pages/clipage.php
+++ app/pages/clipage.php
@@ -1,27 +1,21 @@
 <?php
 
-// echo '<pre>';
-// print_r($_SERVER);
-// echo '</pre>';
+
 $result = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    // $cmd_list = 'echo , assoc,netstat, tracert, PathPing, getmac, netsh, arp, arp -a, ipconfig, time /t, ';
     $command = $_POST['command'];
 
     if ($command) {
         $split_command = explode(" ", $command);
         $permited_commands = ['echo', 'getmac', 'arp'];
         $result = exec($command);
-        // $result = execve($command);
         $not_permitted = ['|', '||', '&', '&&'];
         $exist = false;
         foreach ($not_permitted as $sign) {
-            // echo $sign;
             if (in_array($sign, $split_command)) {
                 $exist = true;
             }
-            // echo '<br>';
         }
 
         if ($exist) {
@@ -36,12 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 
-    // $a = array("red", "green", "blue", "yellow", "brown", "hello", "one");
-    // print_r(array_slice($a, 1, 2));
 
-    // $a = array("red", "green", "blue", "yellow", "brown");
-    // print_r(array_slice($a, 0, 3)); // (array, start num, indexoflastelement+1)
-    // echo "" | more /user/admins 
 }
 ?>
 <style>
@@ -56,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         padding-left: 30px;
         border: none;
         font-size: large;
-        /* border-bottom: 1px solid black; */
+   
     }
 
     div {
