93bae4f8648dac01256833ef0bcd9b8ef1a7b102
Abukiii <abubekerabdu85@gmail.com>
2022-12-07 08:41:36 +0300

    changed the server from local host to the host in docker

--- app/process/db_connection.php
+++ app/process/db_connection.php
@@ -1,19 +1,33 @@
 <?php
 
-// $username = 'crud';
-// $password = 'password';
-// $host = "db";
-// $dbname = 'crud';
-
-$username = 'root';
-$password = '';
-$host = "localhost";
+$username = 'crud';
+$password = 'password';
+$host = "db";
 $dbname = 'crud';
+
+// $username = 'root';
+// $password = '';
+// $host = "localhost";
+// $dbname = 'crud';
 
 $pdo = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$con = mysqli_connect($host, $username, $password, $dbname);
+
+//  // get data from the SQL file
+//  $query = file_get_contents("../db/crud.sql");
+//  // prepare the SQL statements
+//  $stmt = $pdo->prepare($query);
+//  // execute the SQL
+//  if ($stmt->execute()){
+//      echo "Success";
+//  }
+//  else {
+//      echo "Fail";
+//  }
+
+ $con = mysqli_connect($host, $username, $password, $dbname);
 if (!$con) {
     echo 'please check your database connection';
+    
 }
 // echo "Connected";
